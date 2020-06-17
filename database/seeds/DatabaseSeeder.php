0123a55851337a69fa02e5a80d1d365549a37aaa
YaChupa <66978676+YaChupa@users.noreply.github.com>
2020-06-17 22:01:32 +0300

    Images.Design
    8 commit

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -23,6 +23,18 @@ class DatabaseSeeder extends Seeder
         Product::create(array('category_id' => '1','name' => 'Black','code' => 'black','description' => '1234','image' => '1234','price' => '1.8'));
         Product::create(array('category_id' => '1','name' => 'Mint tea','code' => 'mint_tea','description' => '1234','image' => '1234','price' => '1.9'));
         Product::create(array('category_id' => '1','name' => 'Apple and cinnamon tea','code' => 'apple_and_cinnamon_tea','description' => '1234','image' => '1234','price' => '2.0'));
-        Product::create(array('category_id' => '1','name' => 'Tropical fruits','code' => 'tropical_fruits','description' => '1234','image' => '1234','price' => '2.1'));*/
+        Product::create(array('category_id' => '1','name' => 'Tropical fruits','code' => 'tropical_fruits','description' => '1234','image' => '1234','price' => '2.1'));
+        Product::create(array('category_id' => '2','name' => 'Latte','code' => 'latte','description' => '1234','image' => '1234','price' => '2.0'));
+        Product::create(array('category_id' => '2','name' => 'Espresso','code' => 'espresso','description' => '1234','image' => '1234','price' => '1.9'));
+        Product::create(array('category_id' => '2','name' => 'Americano','code' => 'americano','description' => '1234','image' => '1234','price' => '2.1'));
+        Product::create(array('category_id' => '2','name' => 'Cappuccino','code' => 'cappuccino','description' => '1234','image' => '1234','price' => '2.0'));
+        Product::create(array('category_id' => '2','name' => 'Moccaccino','code' => 'moccaccino','description' => '1234','image' => '1234','price' => '2.3'));
+        Product::create(array('category_id' => '2','name' => 'Irish cream','code' => 'irish_cream','description' => '1234','image' => '1234','price' => '1.7'));
+        Product::create(array('category_id' => '3','name' => 'Honey cake','code' => 'honey_cake','description' => '1234','image' => '1234','price' => '2.8'));
+        Product::create(array('category_id' => '3','name' => 'Eclairs','code' => 'eclairs','description' => '1234','image' => '1234','price' => '2.5'));
+        Product::create(array('category_id' => '3','name' => 'Raisin cupcake','code' => 'raisin_cupcake','description' => '1234','image' => '1234','price' => '2.2'));
+        Product::create(array('category_id' => '3','name' => 'Coffee cake','code' => 'coffee_cake','description' => '1234','image' => '1234','price' => '2.4'));
+        Product::create(array('category_id' => '3','name' => 'Sponge cake','code' => 'sponge_cake','description' => '1234','image' => '1234','price' => '2.6'));
+        Product::create(array('category_id' => '3','name' => 'Carrot cake','code' => 'carrot_cake','description' => '1234','image' => '1234','price' => '2.9'));*/
     }
 }
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -4,11 +4,11 @@
 
 @section('content')
 
-<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
-  <div class="col-md-5 p-lg-5 mx-auto my-5">
+<div class="">
+  <div class="">
     <h1 class="display-4 font-weight-normal" style="text-transform:uppercase;">{{$product->name}}</h1>
     <p class="lead font-weight-normal">{{$product->description}}</p>
-    <img src="{{$product->image}}" alt="Girl in a jacket" width="200" height="200">
+    <img src="{{asset($product->image)}}" alt="Girl in a jacket" width="200" height="200">
     <a class="btn btn-outline-secondary" href="{{ route('basket') }}">Add in a basket</a>
     @isset($category)
     {{$category->name}}
